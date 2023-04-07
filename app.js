e4244c42c1e34301578a80434a62aea6f37e463f
SeolHuiGwan9478 <shg5977@naver.com>
2023-04-08 00:33:41 +0900

    [Study] : Express put method

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const members = require('./members');
 
+app.use(express.json());
+
 app.get('/api/members', (req, res) => {
     const { team } = req.query;
     if(team){
@@ -24,7 +26,29 @@ app.get('/api/members/:id', (req, res) => {
     else{
         res.status(404).send({ message : 'There is no such member'});
     }
-})
+});
+
+app.post('/api/members', (req, res) => {
+    const newMember = req.body;
+    members.push(newMember);
+    res.send(newMember);
+});
+
+app.put('/api/members/:id', (req, res) => {
+    const { id } = req.params;
+    const newInfo = req.body;
+    const member = members.find((m) => {
+        m.id === Number(id);
+    })
+    if(member){
+        Object.keys(newInfo).forEach((prop) => {
+            member[prop] = newInfo[prop];
+        })
+        res.send(member);
+    }else{
+        res.status(404).send({ message: "There is no memeber with thd id" });
+    }
+});
 
 app.listen(3000, () => {
     console.log('Server is listening...');
