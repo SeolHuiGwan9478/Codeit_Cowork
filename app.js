fe8a5f12eebcb034b4e97579b81b7c75e60618d8
SeolHuiGwan9478 <shg5977@naver.com>
2023-04-07 15:02:58 +0900

    [Study] : First Commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("test");
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello Express</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening...');
+});
