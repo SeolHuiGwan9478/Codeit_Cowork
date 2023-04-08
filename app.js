c2ca3a3bd8891346bf2f95489a139ce8187b8c29
SeolHuiGwan9478 <shg5977@naver.com>
2023-04-08 21:22:25 +0900

    [Study] : Express GET,POST,PUT,DELETE method

--- app.js
+++ app.js
@@ -2,24 +2,27 @@ const express = require('express');
 
 const app = express();
 
-const members = require('./members');
+const db = require('./models/index');
+
+const { Member } = db;
 
 app.use(express.json());
 
 app.get('/api/members', (req, res) => {
     const { team } = req.query;
     if(team){
-        const teamMembers = members.filter((m) => m.team === team);
+        const teamMembers = Member.findAll({ where: {team} });
         res.send(teamMembers);
     }
     else{
+        const members = Member.findAll();
         res.send(members);
     }
 });
 
-app.get('/api/members/:id', (req, res) => {
+app.get('/api/members/:id', async (req, res) => {
     const { id } = req.params;
-    const member = members.find((m) => m.id === Number(id));
+    const member = await Member.findOne({where:{id}})
     if (member){
         res.send(member);
     }
@@ -28,38 +31,43 @@ app.get('/api/members/:id', (req, res) => {
     }
 });
 
-app.post('/api/members', (req, res) => {
+app.post('/api/members', async (req, res) => {
     const newMember = req.body;
-    members.push(newMember);
-    res.send(newMember);
+    const member = await Member.create(newMember);
+    res.send(member);
 });
 
-app.put('/api/members/:id', (req, res) => {
+// app.put('/api/members/:id', async (req, res) => {
+//     const { id } = req.params;
+//     const newInfo = req.body;
+//     const result = await Member.update(newInfo, {where: {id}});
+//     if(result[0]){
+//         res.send({message: `${result[0]} row(s) affected`});
+//     }else{
+//         res.status(404).send({message: 'There is no member with the id!'});
+//     }
+// });
+
+app.put('/api/members/:id', async (req, res) => {
     const { id } = req.params;
     const newInfo = req.body;
-    const member = members.find((m) => {
-        m.id === Number(id);
-    })
-    if(member){
-        Object.keys(newInfo).forEach((prop) => {
-            member[prop] = newInfo[prop];
-        })
-        res.send(member);
-    }else{
-        res.status(404).send({ message: "There is no memeber with thd id" });
-    }
+    const member = Member.findOne({where: {id}});
+    Object.keys(newInfo).forEach((prop) => {
+        member[prop] = newInfo[prop];
+    });
+    member.save();
+    res.send(member);
 });
 
 app.delete('/api/members/:id', (req, res) => {
     const { id } = req.params;
-    const membersCount = members.length;
-    members = members.filter((member) => member.id !== Number(id))
-    if(members.length < membersCount){
-        res.send({ message: 'Deleted' });
+    const deletedCount = Member.destroy({where: {id}});
+    if(deletedCount){
+        res.send({message: `${deletedCount} row(s) deleted`});
     }else{
-        res.status(404).send({message: 'There is no member with id'});
+        res.status(404).send({message: 'There is no member with the id!'});
     }
-})
+});
 
 app.listen(3000, () => {
     console.log('Server is listening...');
